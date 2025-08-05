6258cdac86286b8ccb8e1a3752565918352b620a
ajiangny <andrewjiang74@gmail.com>
2025-08-04 21:00:02 -0400

    fixed music button interaction with keys, cleaned up gamemap.tsx

---
+++ src/data/photoAssets.ts
@@ -0,0 +1,197 @@
+import day1_1 from "../assets/photos/day1/day1_1.jpg";
+import day1_2 from "../assets/photos/day1/day1_2.jpg";
+import day1_3 from "../assets/photos/day1/day1_3.jpg";
+import day1_4 from "../assets/photos/day1/day1_4.jpg";
+import day1_5 from "../assets/photos/day1/day1_5.jpg";
+import day1_6 from "../assets/photos/day1/day1_6.jpg";
+import day1_7 from "../assets/photos/day1/day1_7.jpg";
+import day1_8 from "../assets/photos/day1/day1_8.jpg";
+import day1_9 from "../assets/photos/day1/day1_9.jpg";
+import day1_10 from "../assets/photos/day1/day1_10.jpg";
+import day2_1 from "../assets/photos/day2/day2_1.jpg";
+import day2_2 from "../assets/photos/day2/day2_2.jpg";
+import day2_3 from "../assets/photos/day2/day2_3.jpg";
+import day2_4 from "../assets/photos/day2/day2_4.jpg";
+import day2_5 from "../assets/photos/day2/day2_5.jpg";
+import day2_6 from "../assets/photos/day2/day2_6.jpg";
+import day2_7 from "../assets/photos/day2/day2_7.jpg";
+import day2_8 from "../assets/photos/day2/day2_8.jpg";
+import day2_9 from "../assets/photos/day2/day2_9.jpg";
+import day2_10 from "../assets/photos/day2/day2_10.jpg";
+import day2_11 from "../assets/photos/day2/day2_11.jpg";
+import day2_12 from "../assets/photos/day2/day2_12.jpg";
+import day3_1 from "../assets/photos/day3/day3_1.jpg";
+import day3_2 from "../assets/photos/day3/day3_2.jpg";
+import day3_3 from "../assets/photos/day3/day3_3.jpg";
+import day3_4 from "../assets/photos/day3/day3_4.jpg";
+import day3_5 from "../assets/photos/day3/day3_5.jpg";
+import day3_6 from "../assets/photos/day3/day3_6.jpg";
+import day3_7 from "../assets/photos/day3/day3_7.jpg";
+import day3_8 from "../assets/photos/day3/day3_8.jpg";
+import day3_9 from "../assets/photos/day3/day3_9.jpg";
+import day3_10 from "../assets/photos/day3/day3_10.jpg";
+import day3_11 from "../assets/photos/day3/day3_11.jpg";
+import day3_12 from "../assets/photos/day3/day3_12.jpg";
+import day4_1 from "../assets/photos/day4/day4_1.jpg";
+import day4_2 from "../assets/photos/day4/day4_2.jpg";
+import day4_3 from "../assets/photos/day4/day4_3.jpg";
+import day4_4 from "../assets/photos/day4/day4_4.jpg";
+import day4_5 from "../assets/photos/day4/day4_5.jpg";
+import day4_6 from "../assets/photos/day4/day4_6.jpg";
+import day4_7 from "../assets/photos/day4/day4_7.jpg";
+import day4_8 from "../assets/photos/day4/day4_8.jpg";
+import day5_1 from "../assets/photos/day5/day5_1.jpg";
+import day5_2 from "../assets/photos/day5/day5_2.jpg";
+import day5_3 from "../assets/photos/day5/day5_3.jpg";
+import day5_4 from "../assets/photos/day5/day5_4.jpg";
+import day5_5 from "../assets/photos/day5/day5_5.jpg";
+import day5_6 from "../assets/photos/day5/day5_6.jpg";
+import day5_7 from "../assets/photos/day5/day5_7.jpg";
+import day5_8 from "../assets/photos/day5/day5_8.jpg";
+import day5_9 from "../assets/photos/day5/day5_9.jpg";
+import day5_10 from "../assets/photos/day5/day5_10.jpg";
+import day5_11 from "../assets/photos/day5/day5_11.jpg";
+import day5_12 from "../assets/photos/day5/day5_12.jpg";
+import day5_13 from "../assets/photos/day5/day5_13.jpg";
+import day5_14 from "../assets/photos/day5/day5_14.jpg";
+import day5_15 from "../assets/photos/day5/day5_15.jpg";
+import day6_1 from "../assets/photos/day6/day6_1.jpg";
+import day6_2 from "../assets/photos/day6/day6_2.jpg";
+import day6_3 from "../assets/photos/day6/day6_3.jpg";
+import day6_4 from "../assets/photos/day6/day6_4.jpg";
+import day6_5 from "../assets/photos/day6/day6_5.jpg";
+import day6_6 from "../assets/photos/day6/day6_6.jpg";
+import day6_7 from "../assets/photos/day6/day6_7.jpg";
+import day6_8 from "../assets/photos/day6/day6_8.jpg";
+import day6_9 from "../assets/photos/day6/day6_9.jpg";
+import day6_10 from "../assets/photos/day6/day6_10.jpg";
+import day6_11 from "../assets/photos/day6/day6_11.jpg";
+import day7_1 from "../assets/photos/day7/day7_1.jpg";
+import day7_2 from "../assets/photos/day7/day7_2.jpg";
+import day7_3 from "../assets/photos/day7/day7_3.jpg";
+import day7_4 from "../assets/photos/day7/day7_4.jpg";
+import day7_5 from "../assets/photos/day7/day7_5.jpg";
+import day7_6 from "../assets/photos/day7/day7_6.jpg";
+import day7_7 from "../assets/photos/day7/day7_7.jpg";
+import day7_8 from "../assets/photos/day7/day7_8.jpg";
+import day7_9 from "../assets/photos/day7/day7_9.jpg";
+import day7_10 from "../assets/photos/day7/day7_10.jpg";
+import day7_11 from "../assets/photos/day7/day7_11.jpg";
+import day7_12 from "../assets/photos/day7/day7_12.jpg";
+import day7_13 from "../assets/photos/day7/day7_13.jpg";
+import day7_14 from "../assets/photos/day7/day7_14.jpg";
+import day7_15 from "../assets/photos/day7/day7_15.jpg";
+
+export const day1Photos = {
+    day1_1,
+    day1_2,
+    day1_3,
+    day1_4,
+    day1_5,
+    day1_6,
+    day1_7,
+    day1_8,
+    day1_9,
+    day1_10
+}
+
+export const day2Photos = {
+    day2_1,
+    day2_2,
+    day2_3,
+    day2_4,
+    day2_5,
+    day2_6,
+    day2_7,
+    day2_8,
+    day2_9,
+    day2_10,
+    day2_11,
+    day2_12
+}
+
+export const day3Photos = {
+    day3_1,
+    day3_2,
+    day3_3,
+    day3_4,
+    day3_5,
+    day3_6,
+    day3_7,
+    day3_8,
+    day3_9,
+    day3_10,
+    day3_11,
+    day3_12
+}
+
+export const day4Photos = {
+    day4_1,
+    day4_2,
+    day4_3,
+    day4_4,
+    day4_5,
+    day4_6,
+    day4_7,
+    day4_8
+}
+
+export const day5Photos = {
+    day5_1,
+    day5_2,
+    day5_3,
+    day5_4,
+    day5_5,
+    day5_6,
+    day5_7,
+    day5_8,
+    day5_9,
+    day5_10,
+    day5_11,
+    day5_12,
+    day5_13,
+    day5_14,
+    day5_15
+}
+
+export const day6Photos = {
+    day6_1,
+    day6_2,
+    day6_3,
+    day6_4,
+    day6_5,
+    day6_6,
+    day6_7,
+    day6_8,
+    day6_9,
+    day6_10,
+    day6_11
+}
+
+export const day7Photos = {
+    day7_1,
+    day7_2,
+    day7_3,
+    day7_4,
+    day7_5,
+    day7_6,
+    day7_7,
+    day7_8,
+    day7_9,
+    day7_10,
+    day7_11,
+    day7_12,
+    day7_13,
+    day7_14,
+    day7_15
+}
+
+export const allPhotoAssets = {
+    ...day1Photos,
+    ...day2Photos,
+    ...day3Photos,
+    ...day4Photos,
+    ...day5Photos,
+    ...day6Photos,
+    ...day7Photos
+}
